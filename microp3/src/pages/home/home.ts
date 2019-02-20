b0bce9ebe54906c307015bea16e61c96f4a8f4c7
Sergio98bm <sergiobm@live.com>
2019-02-20 12:11:35 +0100

    Comit 3.1

---
+++ microp3/src/pages/home/home.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  goToMyPagina(){
+
+    this.navCtrl.push('pagina');
+  }
+
+}
